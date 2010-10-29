368c20dcef193da31815c657b90568064a7d625c
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2010-10-29 19:14:43 +0200

    Merging with emacs-for-python revision 571c7049664a412c7f14

--- init.el
+++ init.el
@@ -26,6 +26,7 @@
 
 (add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit"))
 (add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit/auto-complete"))
+(add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit/emacs-dbgr"))
 
 (setq autoload-file (concat dotfiles-dir "loaddefs.el"))
 (setq package-user-dir (concat dotfiles-dir "elpa"))
--- starter-kit-python.el
+++ starter-kit-python.el
@@ -1,72 +1,294 @@
-;;; 
-(eval-after-load 'python
-  '(progn
-     ;; Ropemacs Settings
-     (setenv "PYTHONPATH"
-             (concat
-              (getenv "PYTHONPATH") ":"
-              (concat dotfiles-dir "/elpa-to-submit/python/rope-dist")))
+;; starter-kit-python.el - setup of python stuff
 
-     (setq pymacs-load-path
-           (list
-            (concat dotfiles-dir "/elpa-to-submit/python/rope-dist/ropemacs/")))
+(defun setup-ropemacs ()
 
-     (pymacs-load "ropemacs" "rope-")
+  "Setup the ropemacs harness"
 
-     ;; Stops from erroring if there's a syntax err
-     (setq ropemacs-codeassist-maxfixes 3)
-     (setq ropemacs-guess-project t)
-     (setq ropemacs-enable-autoimport t)
+  (setenv "PYTHONPATH"
 
-     ;; Adding hook to automatically open a rope project if there is one
-     ;; in the current or in the upper level directory
-     (add-hook 'python-mode-hook
-               (lambda ()
-                 (cond ((file-exists-p ".ropeproject")
-                        (rope-open-project default-directory))
-                       ((file-exists-p "../.ropeproject")
-                        (rope-open-project (concat default-directory "..")))
-                       )))
-     
-     ;; Virtualenv Commands
-     (autoload 'activate-virtualenv "virtualenv" "Activate a Virtual Environment specified by PATH")
-     (autoload 'workon "virtualenv" "Activate a Virtual Environment present using virtualenvwrapper")
+          (concat
 
-     ;; Flymake for python configuration
-     (when (require 'flymake)
-       (defun flymake-epylint-init ()
-         "Epylint flymake python file checking"
-         (when (not (subsetp (list (current-buffer)) (tramp-list-remote-buffers)))
-           (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                              'flymake-create-temp-inplace))
-                  (local-file (file-relative-name
-                               temp-file
-                               (file-name-directory buffer-file-name))))
-             (list "epylint" (list local-file)))))
+           (getenv "PYTHONPATH") ":"
+
+           (concat dotfiles-dir "rope-dist")))
 
 
-       (add-to-list 'flymake-allowed-file-name-masks
-                    '("\\.py\\'" flymake-epylint-init))
-       ;; XXX If flymake is integrated with other modes maybe we should
-       ;; revisite this
-       (add-hook 'python-mode-hook 'flymake-find-file-hook)
 
-       )
-     )
+  ;; TODO: We need something like add-to-list?
+
+  (setq pymacs-load-path
+
+        (list
+
+         (concat dotfiles-dir "python-libs/")))
+
+
+
+  (pymacs-load "ropemacs" "rope-")
+
+  
+
+  ;; Stops from erroring if there's a syntax err
+
+  (setq ropemacs-codeassist-maxfixes 3)
+
+  (setq ropemacs-guess-project t)
+
+  (setq ropemacs-enable-autoimport t)
+
+
+
+  ;; Adding hook to automatically open a rope project if there is one
+
+  ;; in the current or in the upper level directory
+
+  (add-hook 'python-mode-hook
+
+            (lambda ()
+
+              (cond ((file-exists-p ".ropeproject")
+
+                     (rope-open-project default-directory))
+
+                    ((file-exists-p "../.ropeproject")
+
+                     (rope-open-project (concat default-directory "..")))
+
+                    )))
+
   )
 
 
-(add-hook 'python-mode-hook 'turn-on-eldoc-mode)
+
+;; Customizing
+
+(setq flymake-enable-pyflakes t)
+
+(setq flymake-enable-pylint nil)
+
+(setq flymake-enable-pep8 nil)
+
+
+
+;; Python or python mode?
+
+(eval-after-load 'python
+
+  '(progn
+
+     ;;==================================================
+
+     ;; Ropemacs Configuration
+
+     ;;==================================================
+
+     (setup-ropemacs)
+
+
+
+     ;;==================================================
+
+     ;; Virtualenv Commands
+
+     ;;==================================================
+
+     (autoload 'virtualenv-activate "virtualenv"
+
+       "Activate a Virtual Environment specified by PATH" t)
+
+     (autoload 'virtualenv-workon "virtualenv"
+
+       "Activate a Virtual Environment present using virtualenvwrapper" t)
+
+     
+
+     
+
+     ;;==================================================
+
+     ;; Flymake for python configuration
+
+     ;;===================================================
+
+     
+
+     ;; TODO: There is some duplication, that can be removed using macros
+
+     ;; TODO: Implement flymake-remove-checker
+
+
+
+     ;; Instructions to add a new checker based on command:
+
+     ;;
+
+     ;; 1) Write an init function, the flymake-command-setup performs some
+
+     ;;    checks and at the end of the option list the filename to process:
+
+     ;;
+
+     ;;   (defun flymake-newchecker-init ()
+
+     ;;      (flymake-command-setup "command" (list "option1" "option2")))
+
+     ;;
+
+     ;; 2) Use the flymake-add-checker function
+
+     ;;
+
+     ;;    (flymake-add-checker flymake-newchecker-init)
+
+
+
+     (require 'tramp)
+
+     ;; Utilities that increase legibility and reduce code duplication
+
+     (defun current-file-remotep ()
+
+       "Tell if the file is remote"
+
+       (subsetp (list (current-buffer)) (tramp-list-remote-buffers)))
+
+
+
+     (defun flymake-create-copy-file ()
+
+       "Create a copy local file"
+
+       (let* ((temp-file (flymake-init-create-temp-buffer-copy 
+
+                          'flymake-create-temp-inplace)))
+
+         (file-relative-name 
+
+          temp-file 
+
+          (file-name-directory buffer-file-name))))
+
+
+
+     (defun flymake-command-setup (command &optional options)
+
+       "Setup the command to be used with flymake, the command
+
+will be called in this way: COMMAND OPTIONS FILE The FILE varible
+
+is passed after the options."
+
+       ;; Make sure it's not a remote buffer or flymake would not work
+
+       (when (not (current-file-remotep)) 
+
+         (list command
+
+               (append options (list (flymake-create-copy-file))))))
+
+
+
+     (when (require 'flymake "flymake-patch" t)
+
+       (setq flymake-info-line-regex
+
+             (append flymake-info-line-regex '("unused$" "^redefinition" "used$"))))
+
+
+
+     ;; I'm using individual well-defined names to be able to remove them
+
+     ;; in some way
+
+
+
+     ;; Init functions!
+
+     (defun flymake-pyflakes-init ()
+
+       (flymake-command-setup "pyflakes"))
+
+
+
+     (defun flymake-pep8-init ()
+
+       (flymake-command-setup "pep8"))
+
+
+
+     (defun flymake-pylint-init ()
+
+       (flymake-command-setup "python" (list (concat dotfiles-dir "scripts/pylint-mod.py"))))
+
+
+
+     (defun flymake-disable-python-checkers ()
+
+       "Disable all python checkers"
+
+       (dolist (flymake-checker-init '(flymake-pyflakes-init flymake-pep8-init flymake-pylint-init))
+
+         (remove '("\\.py\\'" flymake-checker-init) 'flymake-allowed-file-name-masks)))
+
+
+
+     (defun flymake-add-checker (command)
+
+       "Add the checker specified by the COMMAND list"
+
+       (add-to-list 'flymake-allowed-file-name-masks
+
+                    (list "\\.py\\'" command)))
+
+
+
+     ;; Not on all modes, please
+
+     (add-hook 'python-mode-hook 'flymake-find-file-hook)
+
+
+
+     
+
+     (when flymake-enable-pyflakes
+
+       (flymake-add-checker 'flymake-pyflakes-init))
+
+
+
+     (when flymake-enable-pylint
+
+       (flymake-add-checker 'flymake-pylint-init))
+
+
+
+     (when flymake-enable-pep8
+
+       (flymake-add-checker 'flymake-pep8-init)))
+
+  )
+
+
+
+;; Debugger section, for now I don't bundle it
+
+(require 'dbgr)
+
+
 
 ;; Cython Mode
+
 (autoload 'cython-mode "cython-mode" "Mode for editing Cython source files")
 
+
+
 (add-to-list 'auto-mode-alist '("\\.pyx\\'" . cython-mode))
+
 (add-to-list 'auto-mode-alist '("\\.pxd\\'" . cython-mode))
+
 (add-to-list 'auto-mode-alist '("\\.pxi\\'" . cython-mode))
 
-;; Other files for python mode
-(add-to-list 'auto-mode-alist '("wscript" . python-mode))
-(add-to-list 'auto-mode-alist '("SConscript" . python-mode))
+
 
 (provide 'starter-kit-python)
+
